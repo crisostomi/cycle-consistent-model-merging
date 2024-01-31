77c0852d753ecbaeeb4905ee50e4b288c2a06218
Donato Crisostomi <donatocrisostomi@gmail.com>
2024-01-31 20:15:59 +0000

    add additional flag to shell script

--- shell_scripts/run_all_mergers.sh
+++ shell_scripts/run_all_mergers.sh
@@ -2,14 +2,14 @@
 
 RED='\033[0;31m'
 NC='\033[0m'
-
+additional_flags=$1
 mergers=("dummy" "frank_wolfe_to_universe" "git_rebasin")
 
 for merger in "${mergers[@]}"; do
     echo -e "${RED}Running experiments for merger $merger ${NC}  "
-    python src/scripts/merge_n_models.py "matching/merger=$merger"
+    python src/scripts/merge_n_models.py "matching/merger=$merger" $additional_flags
     for repair_flag in "True" "False"; do
-        python src/scripts/evaluate_merged_model.py "matching/merger=$merger" "matching.repaired=$repair_flag"
+        python src/scripts/evaluate_merged_model.py "matching/merger=$merger" "matching.repaired=$repair_flag" $additional_flags
     done
     echo -e "${RED}Completed experiments for merger $merger ${NC}  "
 done
